2318e5b9a07bd400cbbb5638febab4e6b9a793fe
dtessler2 <154753370+dtessler2@users.noreply.github.com>
2025-02-12 16:28:07 -0500

    Added program to calculate a factorial recursively

--- exercises/calculate_factorial.py
+++ exercises/calculate_factorial.py
@@ -1,6 +1,11 @@
+import logging
+
 # implement factorial_recursive method
 def factorial_recursive(number):
-    pass
+    if number == 0 | number == 1:
+        return 1
+    else:
+        return number * (factorial_recursive(number - 1))
 
 
 def main():
@@ -8,9 +13,13 @@ def main():
     number = int(input("Enter a non-negative integer: "))
 
     # handle negative input
+    while number < 0:
+        logging("Invalid input. The number can not be less than 0.")
+        number = int(input("Enter a non-negative integer: "))
 
     # Call factorial_recursive method
-
+    factorial = factorial_recursive(number)
+    print("The factorial of", number, "is:", factorial)
 
 if __name__ == "__main__":
     main()
