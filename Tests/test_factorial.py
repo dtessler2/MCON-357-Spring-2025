d78116eee86fe41fb2a41fe6615a7681460a5233
dtessler2 <154753370+dtessler2@users.noreply.github.com>
2025-02-13 19:19:56 -0500

    Added error handling and unit testing

---
+++ Tests/test_factorial.py
@@ -0,0 +1,36 @@
+import unittest
+from exercises.calculate_factorial import factorial_recursive, process_input
+
+class test_factorial(unittest.TestCase):
+
+    def test_0_factorial(self):
+        self.assertEqual(factorial_recursive(0), 1)
+
+    def test_1_factorial(self):
+        self.assertEqual(factorial_recursive(1), 1)
+
+    def test_positive(self):
+        self.assertEqual(factorial_recursive(5), 120)
+
+    def test_large_num(self):
+        self.assertEqual(factorial_recursive(15), 1307674368000)
+
+    def test_negative(self):
+        value, error_message = process_input("-7")
+        self.assertNone(value)
+        self.assertEqual(error_message, "Number cannot be negative")
+
+    def test_fraction(self):
+        value, error_message = process_input("3.25")
+        self.assertNone(value)
+        self.assertEqual(error_message, "Number must be an integer")
+
+
+
+
+
+
+    
+        
+
+    
--- exercises/calculate_factorial.py
+++ exercises/calculate_factorial.py
@@ -1,4 +1,23 @@
+import sys
 import logging
+
+def get_user_input():
+    input_string = input("Enter a non-negative integer: ")
+    return input_string
+
+        
+
+def process_input(input_string):
+    error_msg = ""
+    value = None
+    try:
+        value = int(input_string)
+        if value < 0:
+            return None, "Number cannot be negative"
+        return value, error_msg
+    except ValueError as e:
+        error_msg = "Number must be an integer"
+        return None, error_msg
 
 # implement factorial_recursive method
 def factorial_recursive(number):
@@ -7,23 +26,18 @@ def factorial_recursive(number):
     else:
         return number * (factorial_recursive(number - 1))
 
-
 def main():
     print("Factorial Computation Using Recursion")
-    try:
-        number = int(input("Enter a non-negative integer: "))
-
-        # handle negative input
-        while number < 0:
-            logging("Invalid input. The number can not be less than 0.")
-            number = int(input("Enter a non-negative integer: "))
-    except ValueError as e:
-        print("Error: ", e)
-        exit()
-        
+    input = get_user_input()
+    value, error_msg = process_input(input)
     # Call factorial_recursive method
-    factorial = factorial_recursive(number)
-    print("The factorial of", number, "is:", factorial)
+    while value is None:
+        print("Invalid value.")
+        input = get_user_input()
+        value, error_msg = process_input(input)
+
+    factorial = factorial_recursive(value)
+    print("The factorial of", value, "is:", factorial)
 
 if __name__ == "__main__":
     main()
